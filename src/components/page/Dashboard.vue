6b3ca6081ab92a9797480f5b9fb69c6ff4b74528
huzh <huzh@time.com>
2018-08-03 19:18:39 +0800

    商城后台项目初始化提交

---
+++ src/components/page/Dashboard.vue
@@ -0,0 +1,336 @@
+<template>
+    <div>
+        <el-row :gutter="20">
+            <!-- <el-col :span="8">
+                <el-row>
+                    <el-col>
+                        <el-card shadow="hover" class="mgb20">
+                            <div class="user-info">
+                                <img src="static/img/img.jpg" class="user-avator" alt="">
+                                <div class="user-info-cont">
+                                    <div class="user-info-name">{{name}}</div>
+                                    <div>{{role}}</div>
+                                </div>
+                            </div>
+                            <div class="user-info-list">上次登录时间：<span>2018-01-01</span></div>
+                            <div class="user-info-list">上次登录地点：<span>东莞</span></div>
+                        </el-card>
+                        <el-card shadow="hover">
+                            <div slot="header" class="clearfix">
+                                <span>语言详情</span>
+                            </div>
+                            Vue
+                            <el-progress :percentage="57.2" color="#42b983"></el-progress>
+                            JavaScript
+                            <el-progress :percentage="29.8" color="#f1e05a"></el-progress>
+                            CSS
+                            <el-progress :percentage="11.9"></el-progress>
+                            HTML
+                            <el-progress :percentage="1.1" color="#f56c6c"></el-progress>
+                        </el-card>
+                    </el-col>
+                </el-row>
+            </el-col> -->
+            <el-col :span="20">
+                <el-row :gutter="20" class="mgb20">
+                    <el-col :span="20">
+                        <el-card shadow="hover" :body-style="{padding: '0px'}">
+                            <div class="grid-content grid-con-1">
+                                <div class="grid-cont-right">
+                                    <div>代发订单</div>
+                                    <div class="grid-num">1234个</div>
+                                </div>
+                                <div class="grid-cont-right">
+                                    <div>代发订单</div>
+                                    <div class="grid-num">1234个</div>
+                                </div>
+                                <div class="grid-cont-right">
+                                    <div>代发订单</div>
+                                    <div class="grid-num">1234个</div>
+                                </div>
+                                <div class="grid-cont-right">
+                                    <div>代发订单</div>
+                                    <div class="grid-num">1234个</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                    <!-- 
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{padding: '0px'}">
+                            <div class="grid-content grid-con-3">
+                                <i class="el-icon-goods grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">5000</div>
+                                    <div>数量</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col> -->
+                </el-row>
+                <el-row :gutter="20">
+                    <el-col :span="20">
+                        <el-card shadow="hover" :body-style="{padding: '0px',height: '200px'}">
+                            <div class="grid-content grid-con-2">
+                                <i class="el-icon-message grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">321</div>
+                                    <div>系统消息</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                </el-row>
+                <el-row :gutter="20">
+                    <el-col :span="10">
+                    <el-card shadow="hover" :body-style="{ height: '295px'}">
+                        <div slot="header" class="clearfix">
+                            <span>公告</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
+                        </div>
+                        <el-table :data="todoList" :show-header="false" height="500" style="width: 100%;font-size:14px;">
+                            <el-table-column>
+                                <template slot-scope="scope">
+                                    <div class="todo-item" @click="clickDialog">{{scope.row.title}}</div>
+                                </template>
+                            </el-table-column>
+                        </el-table>
+                    </el-card>
+                    </el-col>
+                    <el-col :span="10">
+                    <el-card shadow="hover" :body-style="{ height: '150px'}">
+                        <!-- <div slot="header" class="clearfix">
+                            <span>公告</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
+                        </div>
+                        <el-table :data="todoList" :show-header="false" height="304" style="width: 100%;font-size:14px;">
+                            <el-table-column width="40">
+                                <template slot-scope="scope">
+                                    <el-checkbox v-model="scope.row.status"></el-checkbox>
+                                </template>
+                            </el-table-column>
+                            <el-table-column>
+                                <template slot-scope="scope">
+                                    <el-popover placement="top" title="标题" width="300" height="300" trigger="click">
+                                        <div slot="reference" class="todo-item">{{scope.row.title}}</div>
+                                    </el-popover>
+                                </template>
+                            </el-table-column>
+                            <el-table-column width="60">
+                                <template slot-scope="scope">
+                                    <i class="el-icon-edit"></i>
+                                    <i class="el-icon-delete"></i>
+                                </template>
+                            </el-table-column>
+                        </el-table> -->
+                    </el-card>
+                    </el-col>
+                    <el-col :span="10" class="padding-top-10">
+                    <el-card shadow="hover" :body-style="{ height: '150px'}">
+                        <!-- <div slot="header" class="clearfix">
+                            <span>公告</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
+                        </div> -->
+                        <!-- <el-table :data="todoList" :show-header="false" height="304" style="width: 100%;font-size:14px;">
+                            <el-table-column width="40">
+                                <template slot-scope="scope">
+                                    <el-checkbox v-model="scope.row.status"></el-checkbox>
+                                </template>
+                            </el-table-column>
+                            <el-table-column>
+                                <template slot-scope="scope">
+                                    <el-popover placement="top" title="标题" width="300" height="300" trigger="click">
+                                        <div slot="reference" class="todo-item">{{scope.row.title}}</div>
+                                    </el-popover>
+                                </template>
+                            </el-table-column>
+                            <el-table-column width="60">
+                                <template slot-scope="scope">
+                                    <i class="el-icon-edit"></i>
+                                    <i class="el-icon-delete"></i>
+                                </template>
+                            </el-table-column>
+                        </el-table> -->
+                    </el-card>
+                    </el-col>
+                </el-row>
+            </el-col>
+        </el-row>
+        <el-dialog title="公告详情" :visible.sync="dialogVisible" width="50%" :before-close="handleClose">
+            <span>{{dialogContext}}</span>
+            <span slot="footer" class="dialog-footer">
+                <!-- <el-button @click="dialogVisible = false">取 消</el-button> -->
+                <el-button type="primary" @click="dialogVisible = false">关闭窗口</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "dashboard",
+  data() {
+    return {
+      dialogVisible: false,
+      dialogContext: "",
+      name: localStorage.getItem("ms_username"),
+      todoList: [
+        {
+          title: "今天要修复100个bug",
+          status: false
+        },
+        {
+          title: "今天要修复100个bug",
+          status: false
+        },
+        {
+          title: "今天要写100行代码加几个bug吧",
+          status: false
+        },
+        {
+          title: "今天要修复100个bug",
+          status: false
+        },
+        {
+          title: "今天要修复100个bug",
+          status: true
+        },
+        {
+          title: "今天要写100行代码加几个bug吧",
+          status: true
+        }
+      ]
+    };
+  },
+  methods: {
+    handleClose(done) {
+      this.dialogVisible = true;
+      done();
+    },
+    clickDialog(e) {
+      this.dialogContext = e.target.innerText;
+      this.dialogVisible = true;
+    }
+  },
+  computed: {
+    role() {
+      return this.name === "admin" ? "超级管理员" : "普通用户";
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.padding-top-10 {
+  padding-top: 10px;
+}
+.el-row {
+  margin-bottom: 20px;
+}
+
+.grid-content {
+  display: flex;
+  align-items: center;
+  height: 100px;
+}
+
+.grid-cont-right {
+  flex: 1;
+  text-align: center;
+  font-size: 12px;
+  color: #999;
+  border-right: 1px solid #999;
+}
+
+.grid-content :nth-last-child(1) {
+  border: none;
+}
+
+.grid-num {
+  font-size: 16px;
+  font-weight: normal;
+}
+
+.grid-con-icon {
+  font-size: 50px;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  line-height: 100px;
+  color: #fff;
+}
+
+.grid-con-1 .grid-con-icon {
+  background: rgb(45, 140, 240);
+}
+
+.grid-con-1 .grid-num {
+  color: rgb(14, 9, 8);
+}
+
+.grid-con-2 .grid-con-icon {
+  background: rgb(100, 213, 114);
+}
+
+.grid-con-2 .grid-num {
+  color: rgb(14, 9, 8);
+}
+
+.grid-con-3 .grid-con-icon {
+  background: rgb(242, 94, 67);
+}
+
+.grid-con-3 .grid-num {
+  color: rgb(14, 9, 8);
+}
+
+.user-info {
+  display: flex;
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom: 2px solid #ccc;
+  margin-bottom: 20px;
+}
+
+.user-avator {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+}
+
+.user-info-cont {
+  padding-left: 50px;
+  flex: 1;
+  font-size: 14px;
+  color: #999;
+}
+
+.user-info-cont div:first-child {
+  font-size: 30px;
+  color: #222;
+}
+
+.user-info-list {
+  font-size: 14px;
+  color: #999;
+  line-height: 25px;
+}
+
+.user-info-list span {
+  margin-left: 70px;
+}
+
+.mgb20 {
+  margin-bottom: 20px;
+}
+
+.todo-item {
+  font-size: 14px;
+}
+
+.todo-item-del {
+  text-decoration: line-through;
+  color: #999;
+}
+</style>
---
+++ src/components/page/OnlineManagement.vue
@@ -0,0 +1,318 @@
+<template>
+    <div class="table">
+        <div class="crumbs">
+            <el-breadcrumb separator="/" class="el-breadcrumb">
+                <!-- <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item> -->
+                <!-- <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item> -->
+                <el-breadcrumb-item>出售中</el-breadcrumb-item>
+                <el-breadcrumb-item>已下架</el-breadcrumb-item>
+                <el-breadcrumb-item>审核中</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="handle-box">
+                <!-- <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button> -->
+                <span class="on-offer">出售中</span>
+                <span class="ordering-rule">排序规则：
+                <el-select v-model="select_cate" placeholder="请选择排序规则" class="handle-select mr10">
+                    <!-- <el-option key="1" label="广东省" value="广东省"></el-option>
+                    <el-option key="2" label="湖南省" value="湖南省"></el-option> -->
+                    <el-option key="1" label="默认" value="默认（按上架时间从近至远）"></el-option>
+                    <el-option key="2" label="按总浏览量从高至低" value="按总浏览量从高至"></el-option>
+                    <el-option key="3" label="按总浏览量从低至高" value="按总浏览量从低至"></el-option>
+                    <el-option key="4" label="按销量从高至低" value="按销量从高至低"></el-option>
+                    <el-option key="5" label="" value="按销量从低至高" class="last-el-option"></el-option>
+                </el-select>
+                </span>
+                <span class="promotion-button">
+                    <el-button size="mini" class="promotion-mini">参与促销</el-button>
+                    <el-button size="mini">不参与</el-button>
+                </span>
+                <span class="date-options">
+                    <span class="select-date">选择日期：</span>
+                    <el-date-picker
+                        v-model="dateValue"
+                        type="daterange"
+                        range-separator="~"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期">
+                        </el-date-picker>
+                </span>
+                <span class="search-option">
+                    <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
+                    <el-button type="primary" icon="search" @click="search">搜索</el-button>
+                    <el-button class="clear-button" type="default" icon="search" @click="search">清空</el-button>
+                </span>
+            </div>
+            <el-table :data="data" style="width: 100%"  ref="multipleTable" :header-cell-style="getRowClass" @selection-change="handleSelectionChange">
+                <!-- <el-table-column type="selection" width="55"></el-table-column> -->
+                <!-- <el-table-column prop="date" label="日期" sortable width="150">
+                </el-table-column> -->
+                <el-table-column prop="address" label="商品" :formatter="formatter">
+                </el-table-column>
+                <el-table-column prop="name" label="出售规格" width="100">
+                </el-table-column>
+                <el-table-column prop="address" label="促销计划" :formatter="formatter">
+                </el-table-column>
+                <el-table-column prop="address" label="访问统计" :formatter="formatter">
+                </el-table-column>
+                <el-table-column prop="address" label="销售统计" :formatter="formatter">
+                </el-table-column>
+                <el-table-column prop="address" label="审核提交时间" :formatter="formatter">
+                </el-table-column>
+                <el-table-column label="操作" width="150">
+                    <template slot-scope="scope">
+                        <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                        <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">下架</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="pagination">
+                <!-- <el-pagination  @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange" layout="prev, pager, next" :total="1000">
+                </el-pagination> -->
+                <el-pagination
+                    background
+                    :page-sizes="[10,20,30,40]"
+                    :page-size="10"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="100">
+                </el-pagination>
+            </div>
+        </div>
+
+        <!-- 编辑弹出框 -->
+        <el-dialog title="编辑" :visible.sync="editVisible" width="30%" top="250px">
+            <el-form ref="form" :model="form" label-width="50px">
+                <el-form-item label="日期">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+                </el-form-item>
+                <el-form-item label="姓名">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="地址">
+                    <el-input v-model="form.address"></el-input>
+                </el-form-item>
+
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- 删除提示框 -->
+        <el-dialog title="下架商品" :visible.sync="delVisible" width="400px" center top="300px">
+            <div class="del-dialog-cnt">确定下架以下商品？</div>
+            <div class="del-dialog-goods">{{del_value}}</div>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="delVisible = false">取 消</el-button>
+                <el-button type="primary" @click="deleteRow">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "basetable",
+  data() {
+    return {
+      url: "./static/vuetable.json",
+      dateValue: "",
+      tableData: [],
+      cur_page: 1,
+      multipleSelection: [],
+      select_cate: "",
+      select_word: "",
+      del_list: [],
+      del_value: "",
+      is_search: false,
+      editVisible: false,
+      delVisible: false,
+      form: {
+        name: "",
+        date: "",
+        address: ""
+      },
+      idx: -1
+    };
+  },
+  created() {
+    this.getData();
+  },
+  computed: {
+    data() {
+      return this.tableData.filter(d => {
+        let is_del = false;
+        for (let i = 0; i < this.del_list.length; i++) {
+          if (d.name === this.del_list[i].name) {
+            is_del = true;
+            break;
+          }
+        }
+        if (!is_del) {
+          if (
+            d.address.indexOf(this.select_cate) > -1 &&
+            (d.name.indexOf(this.select_word) > -1 ||
+              d.address.indexOf(this.select_word) > -1)
+          ) {
+            return d;
+          }
+        }
+      });
+    }
+  },
+  methods: {
+    // 分页导航
+    handleCurrentChange(val) {
+      this.cur_page = val;
+      this.getData();
+    },
+    // 获取 easy-mock 的模拟数据
+    getData() {
+      // 开发环境使用 easy-mock 数据，正式环境使用 json 文件
+      if (process.env.NODE_ENV === "development") {
+        this.url = "/ms/table/list";
+      }
+      this.$axios
+        .post(this.url, {
+          page: this.cur_page
+        })
+        .then(res => {
+          this.tableData = res.data.list;
+        });
+    },
+    search() {
+      this.is_search = true;
+    },
+    formatter(row, column) {
+      return row.address;
+    },
+    filterTag(value, row) {
+      return row.tag === value;
+    },
+    handleEdit(index, row) {
+      this.idx = index;
+      const item = this.tableData[index];
+      this.form = {
+        name: item.name,
+        date: item.date,
+        address: item.address
+      };
+      this.editVisible = true;
+    },
+    handleDelete(index, row) {
+      this.idx = index;
+      this.del_value = row.name;
+      this.delVisible = true;
+    },
+    getRowClass({ row, column, rowIndex, columnIndex }) {
+      if (rowIndex == 0) {
+        return "background:#EFEFEF";
+      } else {
+        return "";
+      }
+    },
+    // delAll() {
+    //     const length = this.multipleSelection.length;
+    //     let str = '';
+    //     this.del_list = this.del_list.concat(this.multipleSelection);
+    //     for (let i = 0; i < length; i++) {
+    //         str += this.multipleSelection[i].name + ' ';
+    //     }
+    //     this.$message.error('删除了' + str);
+    //     this.multipleSelection = [];
+    // },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    // 保存编辑
+    saveEdit() {
+      this.$set(this.tableData, this.idx, this.form);
+      this.editVisible = false;
+      this.$message.success(`修改第 ${this.idx + 1} 行成功`);
+    },
+    // 确定删除
+    deleteRow() {
+      this.tableData.splice(this.idx, 1);
+      this.$message.success("下架成功");
+      this.delVisible = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.handle-box {
+  height: 51px;
+  position: relative;
+}
+.on-offer {
+  position: absolute;
+  top: 3px;
+}
+.handle-select {
+  width: 140px;
+}
+.last-el-option{
+    margin-bottom: 15px;
+}
+.handle-input {
+  width: 300px;
+  display: inline-block;
+}
+.del-dialog-cnt {
+  font-size: 16px;
+  text-align: center;
+}
+.del-dialog-goods {
+  margin-top: 10px;
+  color: red;
+  font-size: 18px;
+  text-align: center;
+}
+.el-breadcrumb {
+  margin-left: 30px;
+}
+.crumbs {
+  background: white;
+  width: 100%;
+  padding: 20px 0;
+}
+.ordering-rule {
+  font-weight: 400;
+  font-size: 14px;
+  color: #606266;
+  cursor: text;
+  position: absolute;
+  left: 10%;
+}
+.promotion-button {
+  position: absolute;
+  left: 30%;
+}
+.promotion-mini {
+  margin-right: -16px;
+}
+.search-option {
+  position: absolute;
+  right: 0;
+}
+.clear-button {
+  margin-left: 2px;
+}
+.select-date {
+  font-weight: 400;
+  font-size: 14px;
+  color: #606266;
+  cursor: text;
+}
+.date-options {
+  position: absolute;
+  right: 30%;
+}
+.pagination {
+  text-align: center;
+}
+</style>
